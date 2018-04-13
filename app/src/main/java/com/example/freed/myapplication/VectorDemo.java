7a003b5c3fc9266c4174d61b5f208c6e66148271
liuy <674919909@qq.com>
2018-04-13 21:49:58 +0800

    修改2

--- app/src/main/java/com/example/freed/myapplication/VectorDemo.java
+++ app/src/main/java/com/example/freed/myapplication/VectorDemo.java
@@ -1,13 +1,16 @@
 package com.example.freed.myapplication;
 
+import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class VectorDemo extends AppCompatActivity {
     static {
@@ -17,7 +20,7 @@ public class VectorDemo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_vectordemo);
+        setContentView(R.layout.activity_vectordemo);
     }
 
     public void anim(View v){
@@ -27,6 +30,13 @@ public class VectorDemo extends AppCompatActivity {
         if(drawable instanceof Animatable){
             Log.i("info","doClick1");
             ((Animatable) drawable).start();
+        }
+    }
+    public void switchToL(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            startActivity(new Intent(VectorDemo.this, MainLActivity.class));
+        }else {
+            Toast.makeText(VectorDemo.this, "当前系统不支持L Plus", Toast.LENGTH_LONG).show();
         }
     }
 }
